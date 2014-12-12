54734c9c2b8ba4af527cccd3bb441cc6bad56770
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2014-12-12 12:21:59 +0100

    updating setup classifiers

--- setup.py
+++ setup.py
@@ -10,6 +10,10 @@ setup(
     long_description='This module has been developped by the University of Luxembourg in order to improve the management of its clusters. This application will help you easily deploy an EasyBuild installation and deploy software on it, in a heavily automated way.',
     classifiers=[
     'Programming Language :: Python',
+    'Programming Language :: Python :: 2',
+    'Programming Language :: Python :: 2.5',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
     'Operating System :: Unix',
     'Operating System :: MacOS',
     'Operating System :: POSIX',
