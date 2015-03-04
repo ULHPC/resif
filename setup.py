76fc01d08b55de8aa5e4dbf82a00f19377c625ba
Maxime Schmitt <maxime.schmitt@telecom-bretagne.eu>
2015-03-04 17:39:57 +0100

    Bump v0.9.2: release choice bugfix and EasyBuild v2.0 support (vsc-base)

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='resif',
-    version='0.9.1',
+    version='0.9.2',
     author='ULHPC',
     author_email='hpc-sysadmins@uni.lu',
     maintainer='ULHPC',
