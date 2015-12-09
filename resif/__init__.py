64e93bd942ca27b32541c9bac9b766694812ee02
Sebastien Varrette <sebastien.varrette@uni.lu>
2015-12-09 21:32:20 +0100

    update manifest
    Signed-off-by: Sebastien Varrette <sebastien.varrette@uni.lu>

--- resif/__init__.py
+++ resif/__init__.py
@@ -1,13 +1,18 @@
-"""RESIF (Revolutionary EasyBuild-based Software Installation Framework) is
-designed to easily setup, build a set of software (defined through software
-sets) and deploy them either locally or within a High Performance Computing
-(HPC) facility.
+"""
+    RESIF: Revolutionary EasyBuild-based Software Installation Framework
+    ~~~~~~~
+
+RESIF is designed to easily setup, build a set of software (defined through
+software sets) and deploy them either locally or within a High Performance
+Computing (HPC) facility.
 
 Developed by the Sysadmins Team managing the HPC facility of the University of
 Luxembourg, RESIF is based on [Easybuild](https://hpcugent.github.io/easybuild) and
 [LMod](https://www.tacc.utexas.edu/research-development/tacc-projects/lmod).
 RESIF also permits to rebuild a previously deployed software configuration in
-the attempt to facilitate Reproducible Research."""
+the attempt to facilitate Reproducible Research.
+
+"""
 
 from . import resif
 
